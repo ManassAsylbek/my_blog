8398e94126b9421de59f302f1d2a56143941d40c
Manas Asylbek <asylbekmanass@gmail.com>
2022-07-21 10:27:33 +0600

    add:map

---
+++ src/components/Constants/indexs.js
@@ -0,0 +1,22 @@
+export const tags = [
+    "Travel",'New York','London','IKEA','DIY','Ideas','Baby','Family','News','Clothing','Shopping','Sports','Games'
+]
+export const blog = [
+        {img:'https://www.w3schools.com/w3images/woods.jpg',title:'TITLE HEADING',data:'April 7, 2014',
+            text:'Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat.' +
+                ' Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.' +
+                ' Praesent tincidunt sed' +
+                'tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.'},
+
+        {img:'https://www.w3schools.com/w3images/bridge.jpg',title:'BLOG ENTRY',data:'April 2, 2014',
+            text:'Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat.' +
+                ' Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper.' +
+                ' Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida' +
+                ' diam non fringilla.'}
+    ]
+export const posts = [
+        {img:'https://www.w3schools.com/w3images/workshop.jpg',title:'Lorem',text:'Sed mattis nunc'},
+        {img:'https://www.w3schools.com/w3images/gondol.jpg',title:'Ipsum',text:'Praes tinci sed'},
+        {img:'https://www.w3schools.com/w3images/skies.jpg',title:'Dorum',text:'Ultricies congue'},
+        {img:'https://www.w3schools.com/w3images/rock.jpg',title:'Mingsum',text:'Lorem ipsum dipsum'}
+]
