dd4176e38ff513c496060e1a93e6d5d19dfe60bb
Manas Asylbek <asylbekmanass@gmail.com>
2022-08-09 09:22:34 +0600

    feat:04_08

--- src/pages/Admin/Product/Products.js
+++ src/pages/Admin/Product/Products.js
@@ -20,7 +20,10 @@ const Products = () => {
         }if(e.currentTarget.id==="category"){
             setcategory(e.currentTarget.value)
         }
+    }
 
+    const saveData = () =>{
+        console.log("work")
     }
 
     return (
@@ -33,26 +36,26 @@ const Products = () => {
                 </div>
                 <div>
                     <label htmlFor="imgUrl">Адрес изоброжение</label>
-                    <input type="text" id="img_url"/>
+                    <input type="text" id="imgUrl" onChange={getValue}/>
                 </div>
                 <div>
                     <label htmlFor="price">цена</label>
-                    <input type="number" id="price"/>
+                    <input type="number" id="price" onChange={getValue} />
                 </div>
                 <div>
                     <label htmlFor="desc">Описания</label>
-                    <textarea id="desc"/>
+                    <textarea id="desc" onChange={getValue}/>
                 </div>
                 <div>
                     <label htmlFor="category">Категория</label>
-                    <select id="category">
+                    <select id="category" onChange={getValue}>
                         <option value="0">выберите</option>
                         <option value="1">iphone</option>
                         <option value="2">watches</option>
                     </select>
                 </div>
                 <div>
-                    <button type="submit">Добавить</button>
+                    <button onClick={saveData}>Добавить</button>
                 </div>
 
             </form>
